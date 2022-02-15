ef9bd3566f2eb969a33eded1c15e453d463ec908
Chris Mochinski <cmochinski@gmail.com>
2022-02-14 21:58:27 -0600

    smooth transitions with responsiveness

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { Todo } from "../model";
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdDoneOutline } from "react-icons/md";
+import { CgTrash } from "react-icons/cg";
+import '../App.css';
+
 
 type Props = {
-    todo: Todo,
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
-}
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
-const SingleTodo = ({todo, todos, setTodos}: Props) => {
-  return <div>SingleTodo!!</div>;
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  return (
+    <form className="todoSingle">
+      <h2 className="todoSingleText">{todo.todo}</h2>
+      <div className="iconBlock">
+        <span className="icon">
+          <AiOutlineEdit />
+        </span>
+ 
+        <span className="icon">
+          <CgTrash />
+        </span>
+
+        <span className="icon">
+          <MdDoneOutline />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
